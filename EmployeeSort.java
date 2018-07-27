a02d395014bcd0dc343b4b93ae5b0f3830ec7c59
mendon1996 <41279498+mendon1996@users.noreply.github.com>
2018-07-27 04:58:39 -0700

    Add files via upload

---
+++ EmployeeSort.java
@@ -0,0 +1,22 @@
+package incomeTax;
+
+import java.util.Comparator;
+
+public class EmployeeSort implements Comparator<EmployeeVo> {
+	@Override
+	public int compare(EmployeeVo o1, EmployeeVo o2) {
+		if(o1.getIncometax()>(o2.getIncometax()))// TODO Auto-generated method stub
+		{
+			return -1;  //if -1 it will be in descending order
+		}
+		else
+		{
+			return 1; 
+		}
+}
+
+	
+	
+	
+}
+
