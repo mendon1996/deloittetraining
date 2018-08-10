1eeaefa3578d2eb0e96e0e4d6ef247975d41ada3
mendon1996 <41279498+mendon1996@users.noreply.github.com>
2018-08-10 03:57:40 -0700

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,21 +1,141 @@
-public class Customer {
+package co.deloitte.javatraining.entity;
+
+import java.io.Serializable;
+
+public class Customer implements Serializable {
 	
-	int p = 0;
-	public void disp(int p, int m) {
-		System.out.println("Adding two numbers : " +(p+m));
-		
+	//one line missing here
+	
+	private int customerId;
+	private String customerName;
+	private String customerAddress;
+	private int billAmount;
+	
+	
+	
+	public Customer() {
+	
+	}
+
+
+
+	public Customer(int customerId, String customerName, String customerAddress, int billAmount) {
+		super();
+		this.customerId = customerId;
+		this.customerName = customerName;
+		this.customerAddress = customerAddress;
+		this.billAmount = billAmount;
 	}
 	
-	public void disp() {
-		System.out.println("No addition");
+	
+
+
+
+	public int getCustomerId() {
+		return customerId;
 	}
-	public void disp(int...num) {
-		for(int i:num)
-			System.out.println(i);
+
+
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+
+
+	public int getBillAmount() {
+		return billAmount;
+	}
+
+
+
+	public void setBillAmount(int billAmount) {
+		this.billAmount = billAmount;
 	}
 	
-	public static void main(String[] args) {
-		Customer customer = new Customer();
-		customer.disp(1,2,7,8);
+	
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + billAmount;
+		result = prime * result + ((customerAddress == null) ? 0 : customerAddress.hashCode());
+		result = prime * result + customerId;
+		result = prime * result + ((customerName == null) ? 0 : customerName.hashCode());
+		return result;
 	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (billAmount != other.billAmount)
+			return false;
+		if (customerAddress == null) {
+			if (other.customerAddress != null)
+				return false;
+		} else if (!customerAddress.equals(other.customerAddress))
+			return false;
+		if (customerId != other.customerId)
+			return false;
+		if (customerName == null) {
+			if (other.customerName != null)
+				return false;
+		} else if (!customerName.equals(other.customerName))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", customerName=" + customerName + ", customerAddress="
+				+ customerAddress + ", billAmount=" + billAmount + "]";
+	}
+	
+	
+
+
+	/*@Override
+	public int compareTo(Customer arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}*/
+	
+	
 }
